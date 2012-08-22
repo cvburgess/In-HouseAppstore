f4011fbcbed077765ab96627c9905bdcc704ba69
Charles Burgess <cvburgess@gmx.com>
2012-08-22 17:28:27 -0400

    Initial Commit

---
+++ In-HouseAppstore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  In-HouseAppstore
+//
+//  Created by Charles Burgess on 8/22/12.
+//  Copyright (c) 2012 SquareOne Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ In-HouseAppstore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  In-HouseAppstore
+//
+//  Created by Charles Burgess on 8/22/12.
+//  Copyright (c) 2012 SquareOne Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
